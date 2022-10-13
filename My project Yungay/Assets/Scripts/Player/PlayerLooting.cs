775d79e5229d6e684144a8b9a182c1bb2c6828cb
George Jimy <103298857+KnivesPeans@users.noreply.github.com>
2022-10-12 19:46:02 -0500

    Correction Player Looting

--- My project Yungay/Assets/Scripts/Player/PlayerLooting.cs
+++ My project Yungay/Assets/Scripts/Player/PlayerLooting.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerLooting : MonoBehaviour
 {
+    public Camera cam;
     public Inventory inventory;
     public GameObject lootText;
     // Start is called before the first frame update
@@ -15,36 +16,68 @@ public class PlayerLooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
 
-    }
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-    public void OnTriggerStay(Collider other)
-    {
-        var loot = other.gameObject.GetComponent<Loot>();
+        
 
-        if (loot)
+        if (Physics.Raycast(ray, out hit, 1.5f) && hit.collider.gameObject.GetComponent<Loot>()/*CompareTag("Loot")*/)
         {
+            var loot = hit.collider.gameObject.GetComponent<Loot>();
             lootText.GetComponent<Animator>().SetBool("Show", true);
+
             if (Input.GetKeyDown(KeyCode.F))
             {
                 for (int i = 0; i < loot.loot.Count; i++)
                 {
                     inventory.AddItem(loot.loot[i], loot.loot[i].item, loot.loot[i].amount);
                 }
-
-                lootText.GetComponent<Animator>().SetBool("Show", false);
             }
+
         }
-    }
 
-    private void OnTriggerExit(Collider other)
-    {
-        var loot = other.gameObject.GetComponent<Loot>();
-
-        if (loot)
+        else
         {
             lootText.GetComponent<Animator>().SetBool("Show", false);
-
         }
+            
     }
+
+    //public void OnTriggerStay(Collider other)
+    //{
+    //    var loot = other.gameObject.GetComponent<Loot>();
+
+    //    if (loot)
+    //    {
+    //        lootText.GetComponent<Animator>().SetBool("Show", true);
+    //        if (Input.GetKeyDown(KeyCode.F))
+    //        {
+    //            for (int i = 0; i < loot.loot.Count; i++)
+    //            {
+    //                inventory.AddItem(loot.loot[i], loot.loot[i].item, loot.loot[i].amount);
+    //            }
+
+    //            lootText.GetComponent<Animator>().SetBool("Show", false);
+    //        }
+    //    }
+    //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(cam.transform.position, cam.transform.forward * 1f);
+
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    var loot = other.gameObject.GetComponent<Loot>();
+
+    //    if (loot)
+    //    {
+    //        lootText.GetComponent<Animator>().SetBool("Show", false);
+
+    //    }
+    //}
 }
