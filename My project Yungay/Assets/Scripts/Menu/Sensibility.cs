ca91eedbe174d7067ba8814ad3106c18f02c166e
George Jimy <103298857+KnivesPeans@users.noreply.github.com>
2022-10-15 17:04:16 -0500

    Add Sensibility Value

--- My project Yungay/Assets/Scripts/Menu/Sensibility.cs
+++ My project Yungay/Assets/Scripts/Menu/Sensibility.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Sensibility : MonoBehaviour
 {
     public Slider slider;
     public float slidervalue;
+    public TMP_Text textValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,7 @@ public class Sensibility : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ChangeSlider(float value)
@@ -27,6 +29,15 @@ public class Sensibility : MonoBehaviour
         PlayerPrefs.SetFloat("Sensibility", slidervalue);
         PlayerCam.sensX = slider.value;
         PlayerCam.sensY = slider.value;
+        ShowValue();
 
+    }
+
+    public void ShowValue()
+    {
+        float distanceFromMin = (slider.value - slider.minValue);
+        float sliderRange = (slider.maxValue - slider.minValue);
+        float sliderPercent = (distanceFromMin / sliderRange);
+        textValue.text = sliderPercent.ToString("F2");
     }
 }
