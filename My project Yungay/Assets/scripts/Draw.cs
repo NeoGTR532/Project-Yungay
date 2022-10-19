d5641cb5ae48d1f2859c99eba479ce4a5f0db482
bruno Salas <65439485+BrunoSalas@users.noreply.github.com>
2022-10-18 22:37:09 -0500

    Tutorial part 1

--- My project Yungay/Assets/scripts/Draw.cs
+++ My project Yungay/Assets/scripts/Draw.cs
@@ -7,7 +7,7 @@ public class Draw : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawCube(transform.position,transform.localScale);
+        Gizmos.DrawWireSphere(transform.position,0.5f);
         Gizmos.DrawRay(transform.position,transform.forward*100);
     }
 }
